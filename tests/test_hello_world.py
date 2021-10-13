bc08fa7119ee147ed5b024d2e2340eefafd3015e
Kevin Eifinger <k.eifinger@googlemail.com>
2021-10-13 13:46:22 +0200

    test: Add test for say_bye

--- tests/test_hello_world.py
+++ tests/test_hello_world.py
@@ -7,3 +7,9 @@ def test_say_hello():
     """Test that standard works."""
     world = HelloWorld()
     world.say_hello()
+
+
+def test_say_bye():
+    """Test that standard works."""
+    world = HelloWorld()
+    world.say_bye()
