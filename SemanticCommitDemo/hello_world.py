0a5dadb2bbcfa0e419548c1ee88c1fd90cf6a07e
Kevin Stillhammer <kevin.stillhammer@gmail.com>
2022-01-05 14:29:01 +0100

    feat: Add say_hello_multiple_times

--- SemanticCommitDemo/hello_world.py
+++ SemanticCommitDemo/hello_world.py
@@ -15,6 +15,14 @@ class HelloWorld:
         """Say hello to the world."""
         print(f"Hello {self.name}")
 
+    def say_hello_multiple_times(self) -> None:
+        """Say hello to the world five times."""
+        print(f"Hello {self.name}")
+        print(f"Hello {self.name}")
+        print(f"Hello {self.name}")
+        print(f"Hello {self.name}")
+        print(f"Hello {self.name}")
+
     def say_bye(self) -> None:
         """Say hello to the world."""
         print(f"Goodbye {self.name}")
